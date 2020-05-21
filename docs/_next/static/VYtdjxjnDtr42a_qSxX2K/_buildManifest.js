a4b7f9982640e2c2a87b24eff7756f18ecd73c8f
sondo <dohongson@haj-emp.co.jp>
2020-05-21 09:33:49 +0900

    gennerate site

---
+++ docs/_next/static/VYtdjxjnDtr42a_qSxX2K/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/about":[a],"/index":[a]}}("static\u002Fchunks\u002F00f45954c7289e60eeb83b823c072139938c13cf.2d0e6062e130dfef439f.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
